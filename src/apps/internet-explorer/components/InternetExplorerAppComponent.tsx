503bace0e468d57a20ae3421d3ec06d57f17a7e3
Zihan Huang <1284629091@qq.com>
2025-12-03 15:40:58 -0500

    修复 analytics 导入：InternetExplorer 使用 @vercel/analytics

--- src/apps/internet-explorer/components/InternetExplorerAppComponent.tsx
+++ src/apps/internet-explorer/components/InternetExplorerAppComponent.tsx
@@ -46,7 +46,7 @@ import {
 } from "@/stores/useInternetExplorerStore";
 import FutureSettingsDialog from "@/components/dialogs/FutureSettingsDialog";
 import { useTerminalSounds } from "@/hooks/useTerminalSounds";
-import { track } from "@/utils/analytics";
+import { track } from "@vercel/analytics";
 import { useAppStore } from "@/stores/useAppStore";
 import TimeMachineView from "./TimeMachineView";
 import {
